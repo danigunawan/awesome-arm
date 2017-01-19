a617e1ee76d24494dc6770250efa5be4576a47c8
Denzel Wamburu <denzjoseph@gmail.com>
2017-01-19 14:51:16 +0300

    Add Build for SAMD21J18A

---
+++ led/BUILD/SAMD21J18A/GCC_ARM/main.d
@@ -0,0 +1,143 @@
+BUILD/SAMD21J18A/GCC_ARM/./main.o: main.cpp \
+ BUILD/SAMD21J18A/GCC_ARM/mbed_config.h mbed/c0f6e94411f5/mbed.h \
+ mbed/c0f6e94411f5/platform.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/TARGET_SAMD21J18A/device.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/objects.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/cmsis.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/samd21.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/samd21j18a.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/core_cm0plus.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/core_cmInstr.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/core_cmFunc.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/system_samd21.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_ac.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_adc.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_dac.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_dmac.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_dsu.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_eic.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_evsys.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_gclk.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_hmatrixb.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_i2s.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_mtb.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_nvmctrl.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_pac.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_pm.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_port.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_rtc.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_sercom.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_sysctrl.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_tc.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_tcc.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_usb.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/comp_wdt.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_ac.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_adc.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_dac.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_dmac.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_dsu.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_eic.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_evsys.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_gclk.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_sbmatrix.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_i2s.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_mtb.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_nvmctrl.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_pac0.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_pac1.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_pac2.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_pm.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_port.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_rtc.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_sercom0.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_sercom1.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_sercom2.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_sercom3.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_sercom4.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_sercom5.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_sysctrl.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_tc3.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_tc4.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_tc5.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_tc6.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_tc7.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_tcc0.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_tcc1.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_tcc2.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_usb.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/ins_wdt.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/pio_samd21j18a.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/cmsis_nvic.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/cmsis.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/PortNames.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/TARGET_SAMD21J18A/PeripheralNames.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/compiler.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/common/utils/parts.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/status_codes.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/preprocessor.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/tpaste.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/stringz.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/mrepeat.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/preprocessor.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/mrecursion.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/io.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/common/utils/interrupt.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/common/utils/interrupt/interrupt_sam_nvic.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/TARGET_SAMD21J18A/PinNames.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/gpio_object.h \
+ mbed/c0f6e94411f5/mbed_assert.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/tc/tc.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/system/clock/clock.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/system/clock/gclk.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/system/clock/TARGET_SAMD21/clock_feature.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/system/pinmux/pinmux.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/system/interrupt/system_interrupt.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/system/interrupt/TARGET_SAMD21/system_interrupt_features.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/tcc/tcc.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/adc/adc.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/system/system.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/system/power/TARGET_SAMD21/power.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/system/reset/TARGET_SAMD21/reset.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/adc/TARGET_SAMD21/adc_feature.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/extint/extint.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/extint/extint_callback.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/sercom/i2c/i2c_master.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/sercom/i2c/i2c_common.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/sercom/sercom.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/sercom/sercom_pinout.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/sercom/sercom_interrupt.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/sercom/sercom.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/sercom/i2c/i2c_slave.h \
+ mbed/c0f6e94411f5/dma_api.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/dac/dac.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/drivers/dac/TARGET_SAMD21/dac_feature.h \
+ mbed/c0f6e94411f5/TARGET_SAMD21J18A/TARGET_Atmel/TARGET_SAM_CortexM0P/TARGET_SAMD21J18A/PinNames.h \
+ mbed/c0f6e94411f5/mbed_error.h mbed/c0f6e94411f5/mbed_interface.h \
+ mbed/c0f6e94411f5/DigitalIn.h mbed/c0f6e94411f5/gpio_api.h \
+ mbed/c0f6e94411f5/DigitalOut.h mbed/c0f6e94411f5/DigitalInOut.h \
+ mbed/c0f6e94411f5/BusIn.h mbed/c0f6e94411f5/BusOut.h \
+ mbed/c0f6e94411f5/BusInOut.h mbed/c0f6e94411f5/PortIn.h \
+ mbed/c0f6e94411f5/port_api.h mbed/c0f6e94411f5/PortInOut.h \
+ mbed/c0f6e94411f5/PortOut.h mbed/c0f6e94411f5/AnalogIn.h \
+ mbed/c0f6e94411f5/analogin_api.h mbed/c0f6e94411f5/AnalogOut.h \
+ mbed/c0f6e94411f5/analogout_api.h mbed/c0f6e94411f5/PwmOut.h \
+ mbed/c0f6e94411f5/pwmout_api.h mbed/c0f6e94411f5/Serial.h \
+ mbed/c0f6e94411f5/Stream.h mbed/c0f6e94411f5/FileLike.h \
+ mbed/c0f6e94411f5/FileBase.h mbed/c0f6e94411f5/FileHandle.h \
+ mbed/c0f6e94411f5/SerialBase.h mbed/c0f6e94411f5/FunctionPointer.h \
+ mbed/c0f6e94411f5/serial_api.h mbed/c0f6e94411f5/buffer.h \
+ mbed/c0f6e94411f5/dma_api.h mbed/c0f6e94411f5/CThunk.h \
+ mbed/c0f6e94411f5/SPI.h mbed/c0f6e94411f5/spi_api.h \
+ mbed/c0f6e94411f5/CircularBuffer.h mbed/c0f6e94411f5/Transaction.h \
+ mbed/c0f6e94411f5/SPISlave.h mbed/c0f6e94411f5/I2C.h \
+ mbed/c0f6e94411f5/i2c_api.h mbed/c0f6e94411f5/I2CSlave.h \
+ mbed/c0f6e94411f5/Ethernet.h mbed/c0f6e94411f5/CAN.h \
+ mbed/c0f6e94411f5/RawSerial.h mbed/c0f6e94411f5/Timer.h \
+ mbed/c0f6e94411f5/ticker_api.h mbed/c0f6e94411f5/Ticker.h \
+ mbed/c0f6e94411f5/TimerEvent.h mbed/c0f6e94411f5/us_ticker_api.h \
+ mbed/c0f6e94411f5/Timeout.h mbed/c0f6e94411f5/LowPowerTimeout.h \
+ mbed/c0f6e94411f5/LowPowerTicker.h mbed/c0f6e94411f5/LowPowerTimer.h \
+ mbed/c0f6e94411f5/LocalFileSystem.h mbed/c0f6e94411f5/InterruptIn.h \
+ mbed/c0f6e94411f5/gpio_irq_api.h mbed/c0f6e94411f5/wait_api.h \
+ mbed/c0f6e94411f5/sleep_api.h mbed/c0f6e94411f5/rtc_time.h
