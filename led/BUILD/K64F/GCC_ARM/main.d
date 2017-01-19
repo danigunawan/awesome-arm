29e293f01d369b4a6cbbb44f89140236f6d63af1
Denzel Wamburu <denzjoseph@gmail.com>
2017-01-19 14:35:19 +0300

    Add Led Example

---
+++ led/BUILD/K64F/GCC_ARM/main.d
@@ -0,0 +1,96 @@
+BUILD/K64F/GCC_ARM/./main.o: main.cpp BUILD/K64F/GCC_ARM/mbed_config.h \
+ mbed/c0f6e94411f5/mbed.h mbed/c0f6e94411f5/platform.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/TARGET_FRDM/device.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/objects.h \
+ mbed/c0f6e94411f5/TARGET_K64F/cmsis.h \
+ mbed/c0f6e94411f5/TARGET_K64F/MK64F12.h \
+ mbed/c0f6e94411f5/TARGET_K64F/core_cm4.h \
+ mbed/c0f6e94411f5/TARGET_K64F/core_cmInstr.h \
+ mbed/c0f6e94411f5/TARGET_K64F/core_cmFunc.h \
+ mbed/c0f6e94411f5/TARGET_K64F/core_cmSimd.h \
+ mbed/c0f6e94411f5/TARGET_K64F/system_MK64F12.h \
+ mbed/c0f6e94411f5/TARGET_K64F/cmsis_nvic.h \
+ mbed/c0f6e94411f5/TARGET_K64F/cmsis.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/PortNames.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/TARGET_FRDM/PeripheralNames.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/TARGET_FRDM/PinNames.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/gpio_object.h \
+ mbed/c0f6e94411f5/mbed_assert.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_KPSDK_CODE/hal/gpio/fsl_gpio_hal.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_KPSDK_CODE/hal/gpio/fsl_gpio_features.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/fsl_device_registers.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_adc.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/MK64F12/fsl_bitaccess.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_aips.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_axbs.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_can.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_cau.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_cmp.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_cmt.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_crc.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_dac.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_dma.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_dmamux.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_enet.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_ewm.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_fb.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_fmc.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_ftfe.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_ftm.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_gpio.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_i2c.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_i2s.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_llwu.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_lptmr.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_mcg.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_mcm.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_mpu.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_nv.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_osc.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_pdb.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_pit.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_pmc.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_port.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_rcm.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_rfsys.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_rfvbat.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_rng.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_rtc.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_sdhc.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_sim.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_smc.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_spi.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_uart.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_usb.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_usbdcd.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_vref.h \
+ mbed/c0f6e94411f5/TARGET_K64F/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_MCU_K64F/device/device/MK64F12/MK64F12_wdog.h \
+ mbed/c0f6e94411f5/mbed_error.h mbed/c0f6e94411f5/mbed_interface.h \
+ mbed/c0f6e94411f5/DigitalIn.h mbed/c0f6e94411f5/gpio_api.h \
+ mbed/c0f6e94411f5/DigitalOut.h mbed/c0f6e94411f5/DigitalInOut.h \
+ mbed/c0f6e94411f5/BusIn.h mbed/c0f6e94411f5/BusOut.h \
+ mbed/c0f6e94411f5/BusInOut.h mbed/c0f6e94411f5/PortIn.h \
+ mbed/c0f6e94411f5/port_api.h mbed/c0f6e94411f5/PortInOut.h \
+ mbed/c0f6e94411f5/PortOut.h mbed/c0f6e94411f5/AnalogIn.h \
+ mbed/c0f6e94411f5/analogin_api.h mbed/c0f6e94411f5/AnalogOut.h \
+ mbed/c0f6e94411f5/analogout_api.h mbed/c0f6e94411f5/PwmOut.h \
+ mbed/c0f6e94411f5/pwmout_api.h mbed/c0f6e94411f5/Serial.h \
+ mbed/c0f6e94411f5/Stream.h mbed/c0f6e94411f5/FileLike.h \
+ mbed/c0f6e94411f5/FileBase.h mbed/c0f6e94411f5/FileHandle.h \
+ mbed/c0f6e94411f5/SerialBase.h mbed/c0f6e94411f5/FunctionPointer.h \
+ mbed/c0f6e94411f5/serial_api.h mbed/c0f6e94411f5/buffer.h \
+ mbed/c0f6e94411f5/dma_api.h mbed/c0f6e94411f5/SPI.h \
+ mbed/c0f6e94411f5/spi_api.h mbed/c0f6e94411f5/SPISlave.h \
+ mbed/c0f6e94411f5/I2C.h mbed/c0f6e94411f5/i2c_api.h \
+ mbed/c0f6e94411f5/I2CSlave.h mbed/c0f6e94411f5/Ethernet.h \
+ mbed/c0f6e94411f5/CAN.h mbed/c0f6e94411f5/RawSerial.h \
+ mbed/c0f6e94411f5/Timer.h mbed/c0f6e94411f5/ticker_api.h \
+ mbed/c0f6e94411f5/Ticker.h mbed/c0f6e94411f5/TimerEvent.h \
+ mbed/c0f6e94411f5/us_ticker_api.h mbed/c0f6e94411f5/Timeout.h \
+ mbed/c0f6e94411f5/LowPowerTimeout.h mbed/c0f6e94411f5/LowPowerTicker.h \
+ mbed/c0f6e94411f5/LowPowerTimer.h mbed/c0f6e94411f5/LocalFileSystem.h \
+ mbed/c0f6e94411f5/InterruptIn.h mbed/c0f6e94411f5/gpio_irq_api.h \
+ mbed/c0f6e94411f5/wait_api.h mbed/c0f6e94411f5/sleep_api.h \
+ mbed/c0f6e94411f5/rtc_time.h
